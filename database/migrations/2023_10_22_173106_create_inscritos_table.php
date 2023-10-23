3cac8785edd9a1645eee00d9902d3a96934181b5
DaveFont19 <davidfontes20@gmail.com>
2023-10-22 18:27:33 -0600

    Mi primer commit

---
+++ database/migrations/2023_10_22_173106_create_inscritos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inscritos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_curso');
+            $table->unsignedBigInteger('id_alumno');
+            $table->foreign('id_curso')->references('id')->on('cursos')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_alumno')->references('id')->on('alumnos')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inscritos');
+    }
+};
